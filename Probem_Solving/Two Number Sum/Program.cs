13c59b12556726d2eeae74243b2a3a6edfc30cf0
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-02 14:41:17 +0200

    Initial commit

---
+++ Probem_Solving/Two Number Sum/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Two_Number_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static int[] TwoNumberSum(int[] array, int targetSum)
+        {
+            // Write your code here.
+            Dictionary<int, bool> dict = new Dictionary<int, bool>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (dict.ContainsKey(targetSum - array[i]))
+                {
+                    return new int[2] { array[i], targetSum - array[i] };
+                }
+                else
+                {
+                    dict.Add(array[i], true);
+                }
+            }
+            return new int[0];
+        }
+    }
+}
