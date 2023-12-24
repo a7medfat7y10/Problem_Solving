1a252ef8bf4c579e8f2565275faf364e2eaebee8
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-24 20:52:11 +0200

    Transpose Matrix

---
+++ Probem_Solving/Transpose Matrix/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Transpose_Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] transpose = TransposeMatrix(new int[,] { { 1, 2 }, { 3, 4 } });
+            for (int i = 0; i < transpose.GetLength(0); i++)
+            {
+                for (int j = 0; j < transpose.GetLength(1); j++)
+                {
+                    Console.WriteLine( transpose[i, j]);
+                }
+            }
+        }
+        //O( i * j) time / O(n) space
+        public static int[,] TransposeMatrix(int[,] matrix)
+        {
+            // Write your code here.
+            int rows = matrix.GetLength(0);
+            int cols = matrix.Length / matrix.GetLength(0);
+            int[,] transpose = new int[cols, rows];
+
+            for (int i = 0; i < transpose.GetLength(0); i++)
+            {
+                for (int j = 0; j < transpose.GetLength(1); j++)
+                {
+                    transpose[i, j] = matrix[j, i];
+                }
+            }
+            return transpose;
+        }
+    }
+}
