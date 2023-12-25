b7cdd923c2ebef2ad9eef2e369bae14b7e818b16
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-25 15:38:06 +0200

    Palindrome Check

---
+++ Probem_Solving/Palindrome Check/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Palindrome_Check
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("absdsba"));
+        }
+        public static bool IsPalindrome(string str)
+        {
+            // Write your code here.
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != str[str.Length - 1 - i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
