03361c09117e08b9036db8429d5b472099c44905
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-25 15:33:08 +0200

    Caesar Cipher Encryptor

---
+++ Probem_Solving/Caesar Cipher Encryptor/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Caesar_Cipher_Encryptor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CaesarCypherEncryptor("xyz", 2));
+        }
+        public static string CaesarCypherEncryptor(string str, int key)
+        {
+            // Write your code here.
+            string result = "";
+            key = key % 26;
+            
+            for (int i = 0; i < str.Length; i++)
+            {
+                int charCode = (str[i] - 'a' + key) % 26 + 'a';
+                result += (char) charCode;
+            }
+            return result;
+        }
+    }
+}
