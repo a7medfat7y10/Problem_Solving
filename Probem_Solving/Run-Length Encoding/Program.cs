405f5d8aa4cf6c24f6c4e45bda8516a0eb00865c
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-25 16:22:58 +0200

    Run-Length Encoding

---
+++ Probem_Solving/Run-Length Encoding/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Run_Length_Encoding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RunLengthEncoding("AAAAAAAAAAbbbbbbbbc"));
+        }
+        //O(n) Time / O(n) Space
+        public static string RunLengthEncoding(string str)
+        {
+            // Write your code here.
+            int count = 1;
+            string output = "";
+            string result;
+            for (int i = 1; i < str.Length; i++)
+            {
+                if (str[i] == str[i - 1] && count <9)
+                {
+                    count++;
+                }
+                else
+                {
+                    result = count.ToString() + str[i - 1];
+                    output += result;
+                    count = 1;
+                }
+            }
+            result = count.ToString() + str[str.Length -1];
+            output += result;
+            count = 1;
+
+            return output;
+        }
+    }
+}
