40d43d14993649f0bc570ab869734601503feba7
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-26 17:39:47 +0200

    Generate Document

---
+++ Probem_Solving/Generate Document/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Generate_Document
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GenerateDocument("ahmed", "dameh"));
+        }
+
+        //O(n) time / O(c) space
+        public static bool GenerateDocument(string characters, string document)
+        {
+            // Write your code here.
+            Dictionary<char, int> CountsInCharacters = new Dictionary<char, int>();
+
+            foreach (var c in characters)
+            {
+                if (CountsInCharacters.ContainsKey(c))
+                {
+                    CountsInCharacters[c] += 1;
+                }
+                else
+                {
+                    CountsInCharacters[c] = 1;
+                }
+            }
+
+            foreach (var c in document)
+            {
+                if (!(CountsInCharacters.ContainsKey(c)) || CountsInCharacters[c] == 0)
+                    return false;
+                CountsInCharacters[c] -= 1;
+            }
+
+            return true;
+        }
+    }
+}
