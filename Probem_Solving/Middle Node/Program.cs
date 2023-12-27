7de26b54e76281da6078cc41f83cdda1de0e7444
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-27 20:05:14 +0200

    Middle Node

---
+++ Probem_Solving/Middle Node/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Middle_Node
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List 2");
+        }
+
+        // This is an input class. Do not edit.
+        public class LinkedList
+        {
+            public int value;
+            public LinkedList next;
+
+            public LinkedList(int value)
+            {
+                this.value = value;
+                this.next = null;
+            }
+        }
+        //O(n) time/ O(1) space
+        public LinkedList MiddleNode(LinkedList linkedList)
+        {
+            // Write your code here.
+            int count = 0;
+            LinkedList current = linkedList;
+            while (current.next != null)
+            {
+                count++;
+                current = current.next;
+            }
+            int mid = (count + 1) / 2;
+            count = 0;
+            while (count < mid)
+            {
+                linkedList = linkedList.next;
+                count++;
+            }
+            return linkedList;
+        }
+        //O(n) time/ O(1) space
+        public LinkedList MiddleNodeV2(LinkedList linkedList)
+        {
+            // Write your code here.
+            LinkedList current = linkedList;
+            LinkedList twiceCurrent = linkedList;
+            while (twiceCurrent != null && twiceCurrent.next != null)
+            {
+                current = current.next;
+                twiceCurrent = twiceCurrent.next.next;
+            }
+            return current;
+        }
+    }
+}
