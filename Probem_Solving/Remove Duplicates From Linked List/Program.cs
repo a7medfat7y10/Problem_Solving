dfad22782064b2957aafbb883cd46f664913b9c4
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-27 18:10:10 +0200

    Remove Duplicates From Linked List

---
+++ Probem_Solving/Remove Duplicates From Linked List/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Remove_Duplicates_From_Linked_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List");
+        }
+        // This is an input class. Do not edit.
+        public class LinkedList
+        {
+            public int value;
+            public LinkedList next;
+
+            public LinkedList(int value)
+            {
+                this.value = value;
+                this.next = null;
+            }
+        }
+
+        public LinkedList RemoveDuplicatesFromLinkedList(LinkedList linkedList)
+        {
+            // Write your code here.
+            LinkedList current = linkedList;
+            while (current.next != null)
+            {
+                if (current.value == current.next.value)
+                    current.next = current.next.next;
+                else
+                    current = current.next;
+            }
+            return linkedList;
+        }
+    }
+}
