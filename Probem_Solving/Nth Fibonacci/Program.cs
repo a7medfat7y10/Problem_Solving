6aff9e974568a4d03b9e3d8c50042100f5e2b32d
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-28 03:34:22 +0200

    Nth Fibonacci

---
+++ Probem_Solving/Nth Fibonacci/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Nth_Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetNthFib(6));
+        }
+        public static int GetNthFib(int n)
+        {
+            // Write your code here.
+            if (n == 2)
+                return 1;
+            if (n == 1)
+                return 0;
+            return GetNthFib(n - 1) + GetNthFib(n - 2);
+
+        }
+    }
+}
