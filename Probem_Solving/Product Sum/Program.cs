512b42c55d880ee7b71abf55aee348035636170b
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-31 03:56:44 +0200

    Product Sum

---
+++ Probem_Solving/Product Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Product_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ProductSum(new List<object>() { 1, 3, 4, new List<object>() { 2, -3 }, 3 }));
+        }
+        //O(n) time / O(n) space
+        public static int ProductSum(List<object> array, int depth = 1)
+        {
+            // Write your code here.
+            int sum = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] is IList<object>)
+                {
+                    sum += (int)ProductSum((List<object>)array[i], depth + 1);
+                }
+                else
+                {
+                    sum += (int)array[i];
+                }
+            }
+
+            return sum * depth;
+        }
+    }
+}
