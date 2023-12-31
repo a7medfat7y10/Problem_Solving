fb47d7f1c4208429e0fbbd225c8b611447bbfd28
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-31 11:31:59 +0200

    Insertion Sort

---
+++ Probem_Solving/Insertion Sort/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Insertion_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(InsertionSort(new int[] {1,5,6,3,4,2,9}));
+        }
+        public static int[] InsertionSort(int[] array)
+        {
+            // Write your code here.
+            for (int i = 1; i < array.Length; i++)
+            {
+                int key = array[i];
+                int j = 0;
+
+                for (j = i - 1; j >= 0; --j)
+                {
+                    if ((array[j] > key))
+                    {
+                        array[j + 1] = array[j];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                array[j + 1] = key;
+            }
+            return array;
+        }
+    }
+}
