f9aed6eaf1ab0119a574b8b0a92004147e0e6a3f
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2023-12-31 11:34:20 +0200

    Selection Sort

---
+++ Probem_Solving/Selection Sort/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Selection_Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SelectionSort(new int[] {3,5,4,6,2,1}));
+        }
+        public static int[] SelectionSort(int[] array)
+        {
+            // Write your code here.
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minIndex])
+                        minIndex = j;
+                }
+
+                int temp = array[minIndex];
+                array[minIndex] = array[i];
+                array[i] = temp;
+            }
+            return array;
+        }
+    }
+}
