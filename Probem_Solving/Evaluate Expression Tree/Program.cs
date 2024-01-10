f9419ec550f3e402ddca3e030caf681b6a5d5a71
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-01-10 22:00:44 +0200

    Evaluate Expression Tree

---
+++ Probem_Solving/Evaluate Expression Tree/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Evaluate_Expression_Tree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Evaluate Expression Tree");
+        }
+        public class BinaryTree
+        {
+            public int value;
+            public BinaryTree left = null;
+            public BinaryTree right = null;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        public int EvaluateExpressionTree(BinaryTree tree)
+        {
+            // Write your code here.
+            if (tree.left == null && tree.right == null)
+                return tree.value;
+            int left = EvaluateExpressionTree(tree.left);
+            int right = EvaluateExpressionTree(tree.right);
+            return ChooseOperation(left, right, tree.value);
+        }
+
+        public int ChooseOperation(int num1, int num2, int operand)
+        {
+            if (operand == -1)
+                return num1 + num2;
+            else if (operand == -2)
+                return num1 - num2;
+            else if (operand == -3)
+                return num1 / num2;
+            else if (operand == -4)
+                return num1 * num2;
+            return -1;
+        }
+    }
+}
