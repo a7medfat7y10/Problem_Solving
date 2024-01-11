310b051bbcf3a279bb7ae888fe155a91702ef175
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-01-11 17:30:54 +0200

    Tandem Bicycle

---
+++ Probem_Solving/Tandem Bicycle/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Tandem_Bicycle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] red = new int[] { 2, 3, 1, 7, 5 }; 
+            int[] blue = new int[] { 7, 2, 3, 9, 3 };
+            Console.WriteLine(TandemBicycle(red, blue, true));
+        }
+        public static int TandemBicycle(int[] redShirtSpeeds, int[] blueShirtSpeeds, bool fastest)
+        {
+            Array.Sort(redShirtSpeeds);
+            Array.Sort(blueShirtSpeeds);
+
+            if (fastest == true)
+                Array.Reverse(blueShirtSpeeds);
+
+            int totalSpeed = 0;
+            for (int i = 0; i < blueShirtSpeeds.Length; i++)
+            {
+                if (redShirtSpeeds[i] >= blueShirtSpeeds[i])
+                    totalSpeed += redShirtSpeeds[i];
+                else if (redShirtSpeeds[i] < blueShirtSpeeds[i])
+                    totalSpeed += blueShirtSpeeds[i];
+            }
+
+            return totalSpeed;
+        }
+    }
+}
