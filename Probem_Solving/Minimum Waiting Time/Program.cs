a9aa4cc698c58583726322c659f330dc4a665c0c
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-01-11 17:09:27 +0200

    Minimum Waiting Time

---
+++ Probem_Solving/Minimum Waiting Time/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Minimum_Waiting_Time
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int[] queries = new int[] { 1, 4, 5, 3, 2 };
+            Console.WriteLine(MinimumWaitingTime(queries));
+        }
+        //O(nlog(n)) time / O(1) space
+        public static int MinimumWaitingTime(int[] queries)
+        {
+            // Write your code here.
+            int waitingTime = 0;
+            int sumToNum = 0;
+            Array.Sort(queries);
+            for (int i = 0; i < queries.Length - 1; i++)
+            {
+                sumToNum += queries[i];
+                waitingTime += sumToNum;
+            }
+
+            return waitingTime;
+        }
+    }
+}
