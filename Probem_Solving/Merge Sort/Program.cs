069f514ae608339e8c2c754b3d8e0434a95aa33d
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-01-13 12:43:39 +0200

    Merge Sort

--- Probem_Solving/Merge Sort/Program.cs
+++ Probem_Solving/Merge Sort/Program.cs
@@ -4,11 +4,12 @@
     {
         static void Main(string[] args)
         {
-            int[] arr = MergeSort(new int[] { 6, 7, 5, 3, 5, 7, 8, 2 });
+            int[] arr = MergeSortV2(new int[] { 6, 7, 5, 3, 5, 7, 8, 2 });
             foreach (int i in arr)
                 Console.WriteLine(i);
             
         }
+        //O(nlog(n)) time /O(nlog(n)) space
         public static int[] MergeSort(int[] array)
         {
             // Write your code here.
@@ -68,6 +69,70 @@
             while (j < right_length)
             {
                 arr[k] = Right_Array[j];
+                j++;
+                k++;
+            }
+        }
+
+
+
+        //Another optimized solution 
+        //O(nlog(n)) time /O(n) space
+        //using only another copy of the array(auxiliary array)
+        public static int[] MergeSortV2(int[] array)
+        {
+            // Write your code here.
+            if (array.Length <= 0)
+                return array;
+            int[] auxiliaryArray = (int[])array.Clone();
+
+            MergeSort(array, 0, array.Length - 1, auxiliaryArray);
+            return array;
+        }
+        public static int[] MergeSort(int[] arr, int left, int right, int[] auxiliaryArr)
+        {
+            if (left < right)
+            {
+                int mid = (left + right) / 2;
+
+                MergeSort(auxiliaryArr, left, mid,arr );
+                MergeSort(auxiliaryArr, mid + 1, right, arr);
+
+                Merge(arr, left, mid, right, auxiliaryArr);
+            }
+            return arr;
+        }
+
+        private static void Merge(int[] arr, int left, int mid, int right, int[] auxiliaryArr)
+        {
+            int i = left, j = mid + 1;
+            int k =left ;
+
+            while (i <= mid && j <= right)
+            {
+                if (auxiliaryArr[i] <= auxiliaryArr[j])
+                {
+                    arr[k] = auxiliaryArr[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = auxiliaryArr[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i <= mid)
+            {
+                arr[k] = auxiliaryArr[i];
+                i++;
+                k++;
+            }
+
+            while (j <= right)
+            {
+                arr[k] = auxiliaryArr[j];
                 j++;
                 k++;
             }
