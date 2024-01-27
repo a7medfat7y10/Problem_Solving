422e978dbca5578aa898bce205f761c830f98cf8
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-01-27 17:56:25 +0200

    Majority Element

---
+++ Probem_Solving/Majority Element/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Majority_Element
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(MajorityElement(new int[] { 1,3,3,2,2,2,5 }));
+        }
+        //O(n) time / O(1) space
+        public static int MajorityElement(int[] array)
+        {
+            // Write your code here.
+            int count = 0;
+            int majorElement = 0;
+            //if count = 0 this means that there is no major element in the provious part of the array
+            foreach (int value in array)
+            {
+                if (count == 0)
+                    majorElement = value;
+                if (value == majorElement)
+                    count++;
+                else
+                    count--;
+            }
+            return majorElement;
+        }
+    }
+}
