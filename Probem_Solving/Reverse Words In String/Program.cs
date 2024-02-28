250da3685b8515a906d87b8d396e6dfafa0c180e
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-02-28 08:56:22 +0200

    Reverse Words In String

---
+++ Probem_Solving/Reverse Words In String/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Reverse_Words_In_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReverseWordsInString("Hello, World!"));
+        }
+        //O(n) time / O(n) space
+        public static string ReverseWordsInString(string str)
+        {
+            // Write your code here.
+            List<string> words = new List<string>();
+            int startOfWord = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    words.Add(str.Substring(startOfWord, i - startOfWord));
+                    startOfWord = i;
+                }
+                else if (str[startOfWord] == ' ')
+                {
+                    words.Add(" ");
+                    startOfWord = i;
+                }
+            }
+            //add the last word
+            words.Add(str.Substring(startOfWord));
+
+            words.Reverse();
+
+            string word = "";
+            foreach (string w in words)
+            {
+                word += w;
+            }
+            return word;
+        }
+    }
+}
