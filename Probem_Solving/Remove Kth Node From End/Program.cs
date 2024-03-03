2f6315cbb2f51471018b8cde901ac9e7a3bfefb2
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-03 13:48:17 +0200

    Remove Kth Node From End

--- Probem_Solving/Remove Kth Node From End/Program.cs
+++ Probem_Solving/Remove Kth Node From End/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Remove Kth Node From End");
+        }
+        public static void RemoveKthNodeFromEnd(LinkedList head, int k)
+        {
+            // Write your code here.
+            LinkedList fast = head;
+            LinkedList slow = head;
+
+            int counter = 0;
+            while (counter < k)
+            {
+                fast = fast.Next;
+                counter++;
+            }
+            if (fast == null)
+            { // this means head is the one to remove
+                head.Value = head.Next.Value;
+                head.Next = head.Next.Next;
+                return;
+            }
+            while (fast.Next != null)
+            {
+                slow = slow.Next;
+                fast = fast.Next;
+            }
+            slow.Next = slow.Next.Next;
+        }
+
+        public class LinkedList
+        {
+            public int Value;
+            public LinkedList Next = null;
+
+            public LinkedList(int value)
+            {
+                this.Value = value;
+            }
         }
     }
 }
