a13468c3fa1019d73a337f6f6c61feae91909e7c
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-03 17:47:45 +0200

    Merging Linked Lists

---
+++ Probem_Solving/Merging Linked Lists/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Merging_Linked_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Merging Linked Lists");
+        }
+        //O(n+m) time / O(1) space 
+        public LinkedList MergingLinkedLists(LinkedList linkedListOne, LinkedList linkedListTwo){
+            // Write your code here.
+            //pointer will traverse linkedlist one then two
+            LinkedList ptr1 = linkedListOne;
+            //pointer will traverse linkedlist two then one
+            LinkedList ptr2 = linkedListTwo;
+            while (ptr1 != ptr2)
+            {
+                if (ptr1 == null)
+                    ptr1 = linkedListTwo;
+                else
+                    ptr1 = ptr1.next;
+                if (ptr2 == null)
+                    ptr2 = linkedListOne;
+                else
+                    ptr2 = ptr2.next;
+            }
+            return ptr1;
+        }
+    }
+    public class LinkedList
+    {
+        public int value;
+        public LinkedList next;
+
+        public LinkedList(int value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+    }
+}
