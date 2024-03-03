7621df9278b38275e6a7ca8932e88465c8ece4f4
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-03 15:38:51 +0200

    Sum Of Linked Lists

---
+++ Probem_Solving/Sum Of Linked Lists/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Sum_Of_Linked_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum Of Linked Lists");
+        }
+        //O(n) time / O(n) space
+        public LinkedList SumOfLinkedLists(
+          LinkedList linkedListOne, LinkedList linkedListTwo
+        )
+        {
+            // Write your code here.
+            int sum = 0;
+            int i = 1;
+            while (linkedListOne != null)
+            {
+                sum += linkedListOne.value * i;
+                i *= 10;
+                linkedListOne = linkedListOne.next;
+            }
+            i = 1;
+            while (linkedListTwo != null)
+            {
+                sum += linkedListTwo.value * i;
+                i *= 10;
+                linkedListTwo = linkedListTwo.next;
+            }
+            string sumStr = sum.ToString();
+            LinkedList result = new LinkedList(Convert.ToInt32(sumStr[sumStr.Length - 1] - 48));
+            LinkedList node = result;
+            for (i = 1; i < sumStr.Length; i++)
+            {
+                node.next = new LinkedList(Convert.ToInt32(sumStr[sumStr.Length - 1 - i] - 48));
+                node = node.next;
+            }
+            return result;
+        }
+        public class LinkedList
+        {
+            public int value;
+            public LinkedList next;
+
+            public LinkedList(int value)
+            {
+                this.value = value;
+                this.next = null;
+            }
+        }
+
+    }
+}
