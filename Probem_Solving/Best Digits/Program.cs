fad86973aaced986333df81000b55b142b63a144
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-05 23:26:45 +0200

    Best Digits

---
+++ Probem_Solving/Best Digits/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+
+namespace Best_Digits
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BestDigits("682345" , 2));
+        }
+        //O(n) time / O(n) space
+        public static string BestDigits(string number, int numDigits)
+        {
+            // Write your code here.
+            Stack<char> stack = new Stack<char>();
+            for (int i = 0; i < number.Length; i++)
+            {
+                char character = number[i];
+                while (stack.Count > 0 && numDigits > 0 && character > stack.Peek())
+                {
+                    numDigits--;
+                    stack.Pop();
+                }
+                stack.Push(character);
+            }
+            while (numDigits > 0)
+            {
+                numDigits--;
+                stack.Pop();
+            }
+            StringBuilder result = new StringBuilder();
+            while (stack.Count > 0)
+            {
+                result.Append(stack.Pop());
+            }
+            var charArray = result.ToString().ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
