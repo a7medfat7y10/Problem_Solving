83be0c4ccd371d9dfadd69eb39a5bf594aa2940b
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-09 15:00:36 +0200

    Sort Stack

---
+++ Probem_Solving/Sort Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Sort_Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach(var num in SortStack(new List<int> { 1, -2, 5, 3, -5 }))
+            {
+                Console.WriteLine(num);
+            }
+            
+        }
+        //O(n^2) time / O(n) space
+        public static List<int> SortStack(List<int> stack)
+        {
+            // Write your code here.
+            if (stack.Count == 0)
+                return stack;
+            int top = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            SortStack(stack);
+            insertInSortedOrder(stack, top);
+
+            return stack;
+        }
+        public static void insertInSortedOrder(List<int> stack, int num)
+        {
+            // Write your code here.
+            if (stack.Count == 0 || stack[stack.Count - 1] <= num)
+            {
+                stack.Add(num);
+                return;
+            }
+            int top = stack[stack.Count - 1];
+            stack.RemoveAt(stack.Count - 1);
+            insertInSortedOrder(stack, num);
+            stack.Add(top);
+        }
+    }
+}
