1dc3618d34947eab3e97c6af1bb7ec7520a4b868
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-09 20:20:16 +0200

    Reverse Polish Notation

---
+++ Probem_Solving/Reverse Polish Notation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Reverse_Polish_Notation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ReversePolishNotation(new string[] {"10" , "3" , "-" , "7" , "/"}));
+        }
+        //O(n) time / O(n) space
+        public static int ReversePolishNotation(string[] tokens)
+        {
+            // Write your code here.
+            Stack<int> stack = new Stack<int>();
+            foreach (string token in tokens)
+            {
+                if (token == "+")
+                    stack.Push(stack.Pop() + stack.Pop());
+                else if (token == "-")
+                    stack.Push(-1 * (stack.Pop() - stack.Pop()));
+                else if (token == "*")
+                    stack.Push(stack.Pop() * stack.Pop());
+                else if (token == "/") {
+                    int firstNum = stack.Pop();
+                    stack.Push(stack.Pop() / firstNum);
+                }
+                else
+                    stack.Push(int.Parse(token));
+            }
+            return stack.Pop();
+        }
+    }
+}
