a376c256b0976f2195d337058dedfdd89b85e1a2
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-10 18:44:58 +0200

    Binary Tree Diameter

---
+++ Probem_Solving/Binary Tree Diameter/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Binary_Tree_Diameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Binary Tree Diameter");
+        }
+        //O(n) time / O(h) space where h is the height of the tree
+        public int BinaryTreeDiameter(BinaryTree tree)
+        {
+            // Write your code here.
+            return getTreeInfo(tree).diameter;
+        }
+
+        public TreeInfo getTreeInfo(BinaryTree tree)
+        {
+            if (tree == null)
+                return new TreeInfo(0, 0);
+            TreeInfo leftTreeInfo = getTreeInfo(tree.left);
+            TreeInfo rightTreeInfo = getTreeInfo(tree.right);
+
+            int longestPath = leftTreeInfo.height + rightTreeInfo.height;
+            int maxDiameter = Math.Max(leftTreeInfo.diameter, rightTreeInfo.diameter);
+            int currentDiameter = Math.Max(longestPath, maxDiameter);
+            int currentHeight = 1 + Math.Max(leftTreeInfo.height, rightTreeInfo.height);
+            return new TreeInfo(currentDiameter, currentHeight);
+        }
+        public class TreeInfo
+        {
+            public int diameter;
+            public int height;
+            public TreeInfo(int diameter, int height)
+            {
+                this.diameter = diameter;
+                this.height = height;
+            }
+        }
+
+        public class BinaryTree
+        {
+            public int value;
+            public BinaryTree left;
+            public BinaryTree right;
+
+            public BinaryTree(int value)
+            {
+                this.value = value;
+            }
+        }
+    }
+}
