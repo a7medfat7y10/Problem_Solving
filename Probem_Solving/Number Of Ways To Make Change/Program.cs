b7c0ad6b63161165f70cb4a141120ee4077c57d5
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-10 15:27:00 +0200

    Number Of Ways To Make Change

---
+++ Probem_Solving/Number Of Ways To Make Change/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Number_Of_Ways_To_Make_Change
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumberOfWaysToMakeChange(6, new int[] { 1, 5 }));
+        }
+        //O(n) time / O(n) space
+        public static int NumberOfWaysToMakeChange(int n, int[] denoms)
+        {
+            // Write your code here.
+            int[] ways = new int[n + 1];
+            ways[0] = 1;
+            foreach (var denom in denoms)
+            {
+                //store at each amount i the number of ways that the denoms can make this amount
+                for (int i = 1; i <= n; i++)
+                {
+                    if (denom <= i)
+                    {
+                        ways[i] += ways[i - denom];
+                    }
+                }
+            }
+            return ways[n];
+        }
+    }
+}
