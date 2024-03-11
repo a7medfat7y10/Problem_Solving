37076c96bd3b1811212224cfa0b190a17564ae71
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-11 15:06:00 +0200

    Levenshtein Distance

---
+++ Probem_Solving/Levenshtein Distance/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Levenshtein_Distance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LevenshteinDistance("abd", "yabc"));
+        }
+        public static int LevenshteinDistance(string str1, string str2)
+        {
+            // Write your code here.
+            int[,] editsTable = new int[str2.Length + 1, str1.Length + 1];
+            for (int i = 0; i < str2.Length + 1; i++)
+            {
+                for (int j = 0; j < str1.Length + 1; j++)
+                {
+                    editsTable[i, j] = j;
+                }
+                //the first row which is the empty string edits to turn to the first string 
+                editsTable[i, 0] = i;
+            }
+            for (int i = 1; i < str2.Length + 1; i++)
+            {
+                for (int j = 1; j < str1.Length + 1; j++)
+                {
+                    if (str2[i - 1] == str1[j - 1])
+                    {
+                        editsTable[i, j] = editsTable[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        editsTable[i, j] = 1 + Math.Min(editsTable[i - 1, j - 1],
+                                                        Math.Min(editsTable[i - 1, j], editsTable[i, j - 1]));
+                    }
+                }
+            }
+            return editsTable[str2.Length, str1.Length];
+        }
+    }
+}
