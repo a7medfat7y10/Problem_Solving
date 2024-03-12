0039bae9671ca8f317130da7e4c78ca3210854fa
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-12 15:15:19 +0200

    Validate BST

---
+++ Probem_Solving/Validate BST/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Validate_BST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Validate BST");
+        }
+        //O(n) time / O(h) space
+        public static bool ValidateBst(BST tree)
+        {
+            // Write your code here.
+            return ValidateBst(tree, int.MinValue, int.MaxValue);
+        }
+
+        public static bool ValidateBst(BST tree, int min, int max)
+        {
+            if (tree == null)
+                return true;
+            return
+                ValidateBst(tree.left, min, tree.value) &&
+                ValidateBst(tree.right, tree.value, max) &&
+                tree.value >= min && tree.value < max;
+        }
+        public class BST
+        {
+            public int value;
+            public BST left;
+            public BST right;
+
+            public BST(int value)
+            {
+                this.value = value;
+            }
+        }
+    }
+}
