97b03ddf84a0e23569945640a874e7608968f486
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-15 05:50:13 +0200

    Reconstruct BST

---
+++ Probem_Solving/Reconstruct BST/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Reconstruct_BST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reconstruct BST");
+        }
+        public class BST
+        {
+            public int value;
+            public BST left = null;
+            public BST right = null;
+
+            public BST(int value)
+            {
+                this.value = value;
+            }
+        }
+
+        public class TreeInfo
+        {
+            public int rootIndx;
+            public TreeInfo(int i)
+            {
+                this.rootIndx = i;
+            }
+        }
+        //O(n) time / O(n) space
+        public BST ReconstructBst(List<int> preOrderTraversalValues)
+        {
+            // Write your code here.
+            TreeInfo info = new TreeInfo(0);
+            return ReconstructBst(preOrderTraversalValues, int.MinValue, int.MaxValue, info);
+        }
+        public BST ReconstructBst(List<int> preOrderTraversalValues, int lower, int upper, TreeInfo subTreeInfo)
+        {
+            // Write your code here.
+            if (subTreeInfo.rootIndx == preOrderTraversalValues.Count)
+                return null;
+            int root = preOrderTraversalValues[subTreeInfo.rootIndx];
+            if (root < lower || root >= upper)
+                return null;
+
+            subTreeInfo.rootIndx++;
+            BST left = ReconstructBst(preOrderTraversalValues, lower, root, subTreeInfo);
+            BST right = ReconstructBst(preOrderTraversalValues, root, upper, subTreeInfo);
+
+            BST bst = new BST(root);
+            bst.left = left;
+            bst.right = right;
+
+            return bst;
+        }
+    }
+}
