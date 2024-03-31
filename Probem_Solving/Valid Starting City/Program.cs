c621352c75a3280a55c165fd21f42567daaf1d12
a7medfat7y10 <ahmedfathy.1074@gmail.com>
2024-03-31 21:21:20 +0200

    Valid Starting City

---
+++ Probem_Solving/Valid Starting City/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Valid_Starting_City
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ValidStartingCity(new int[] {5,25,15,10,15}, new int[] {1,2,1,0,3}, 10));
+        }
+        //O(n) time / O(1) space
+        public static int ValidStartingCity(int[] distances, int[] fuel, int mpg)
+        {
+            // Write your code here.
+            //the starting city should be the city with the most miles remaining
+            int milesInTank = 0;
+            //start from city 0
+            int startingCityIdx = 0;
+            int milesRemainingAtStartingCity = 0;
+            for (int i = 1; i < distances.Length; i++)
+            {
+                milesInTank += fuel[i - 1] * mpg - distances[i - 1];
+                //if the fuel is not enough
+                if (milesInTank < milesRemainingAtStartingCity)
+                {
+                    milesRemainingAtStartingCity = milesInTank;
+                    startingCityIdx = i;
+                }
+            }
+            return startingCityIdx;
+        }
+    }
+}
